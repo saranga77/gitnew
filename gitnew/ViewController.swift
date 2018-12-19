8fd0e43ddbbcb0c19eb5bd48ccb6ee2ed8d4aa8b
saranga geeganage <sarangageeganage@sarangas-macbook-pro.local>
2018-12-19 17:53:18 +0530

    Initial Commit

---
+++ gitnew/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitnew
+//
+//  Created by saranga geeganage on 12/19/18.
+//  Copyright © 2018 saranga geeganage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
